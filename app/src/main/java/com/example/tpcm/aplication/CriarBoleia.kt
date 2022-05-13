a461ac0753fc543707d831a6263caa9f5f505e1f
carlosdcramos <carlosdcramos@gmail.com>
2022-05-13 14:47:52 +0100

    CriarBoleia

---
+++ app/src/main/java/com/example/tpcm/aplication/CriarBoleia.kt
@@ -0,0 +1,12 @@
+package com.example.tpcm.aplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tpcm.R
+
+class CriarBoleia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criar_boleia)
+    }
+}
