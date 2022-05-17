c073ebe319ee3ee7899f7cfefd942b1f2c45483f
José Luís Pereira <exemplo@email.com>
2022-05-17 13:23:41 +0100

    Página Perfil não habilitado a boleias

---
+++ app/src/main/java/com/example/tpcm/aplication/AddBoleiaNaoHabilitado.kt
@@ -0,0 +1,12 @@
+package com.example.tpcm.aplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tpcm.R
+
+class AddBoleiaNaoHabilitado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_boleia_nao_habilitado)
+    }
+}
