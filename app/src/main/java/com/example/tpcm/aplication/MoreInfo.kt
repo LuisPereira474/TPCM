e770274830739501a8d2a64b4f7a4dfa8e4d2bd0
José Luís Pereira <exemplo@email.com>
2022-06-04 17:48:55 +0100

    Edit layout More Info and Boleia Condutor

---
+++ app/src/main/java/com/example/tpcm/aplication/MoreInfo.kt
@@ -0,0 +1,12 @@
+package com.example.tpcm.aplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tpcm.R
+
+class MoreInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more_info)
+    }
+}
