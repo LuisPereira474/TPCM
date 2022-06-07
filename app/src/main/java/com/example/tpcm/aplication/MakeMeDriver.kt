6b6f12109c069d953ba2f67b3a15ba3223f45fe5
José Luís Pereira <exemplo@email.com>
2022-06-07 19:09:50 +0100

    minor update

--- app/src/main/java/com/example/tpcm/aplication/MakeMeDriver.kt
+++ app/src/main/java/com/example/tpcm/aplication/MakeMeDriver.kt
@@ -2,14 +2,12 @@ package com.example.tpcm.aplication
 
 import android.content.Intent
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.view.ViewGroup
 import android.widget.Button
